dfe9a980eb2104555dc94e8709d88b36e61405bb
Fernando <fernandomorenosilva10@gmail.com>
2024-09-25 11:33:37 -0300

    exercicios js

---
+++ exercicios/ex003/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha\n segunda linha");
+console.log(" O meu nome e 'Fernado");
+console.log('O nome dela e "Jessica');
+console.log(`A multiplicaçao de 5 por 3 e ${5*3}`);
+console.log("O " + "Meu" + "nome" + "e" + "Fernando");
